fea79ab6def855a1599d35b56da46828395461d2
Rob Clark <clarkrob13@gmail.com>
2021-02-22 09:20:23 -0500

    Customer model created with all properties

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace TrashCollector.Models
 {
     public class Customer
     {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public string PickupDay { get; set; }
+        public DateTime SuspendServiceStart { get; set; }
+        public DateTime SuspendServiceEnd { get; set; }
+        public DateTime AdditionalPickupDay { get; set; }
+        public int BalanceDue { get; set; }
     }
 }
